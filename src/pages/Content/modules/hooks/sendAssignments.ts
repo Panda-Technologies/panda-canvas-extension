7330676465612e8ee71a3f30b86442d541589b25
HendersonDaniel <danielhenderson.codeprojects@gmail.com>
2025-01-03 16:15:39 -0500

    Integrated api call to post courses from canvas to ramvisor backend

--- src/pages/Content/modules/hooks/sendAssignments.ts
+++ src/pages/Content/modules/hooks/sendAssignments.ts
@@ -1,25 +1,35 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Course from '../types/course';
 import {getCourses} from './useCourses';
 import { getAllAssignmentsRequest } from './useAssignments';
 
 
 async function sendCourses() {
-    const courses: Course[] = await getCourses();
-  
-    // Map each Course object to the desired JSON format
-    const classSection = courses.map((course) => ({
-      classCode: course.course_code?.split('.')[0] || course.name.split('.')[0], // Use `course_code` if available, otherwise fallback to `name`
-      color: course.color,
-      sectionId: course.name.split('.')[1], 
-      semester: course.name.split('.')[2],
-      
-    }));
-  
-    console.log(classSection); 
-    
-    const outPutString = JSON.stringify(classSection)
+  const courses: Course[] = await getCourses();
 
+  // Map each Course object to the desired JSON format
+  const classSection = courses.map((course) => ({
+    classCode: course.course_code?.split('.')[0] || course.name.split('.')[0], // Use `course_code` if available, otherwise fallback to `name`
+    color: course.color,
+    sectionId: course.name.split('.')[1], 
+    semesterId: course.name.split('.')[2],
+    
+  }));
+
+  //console.log(classSection); 
+
+  try {
+    const importedClasses = await importCanvasClasses(classSection);
+    console.log('Imported Classes:', importedClasses);
+    return importedClasses;
+  } catch (error) {
+    console.error('Failed to send courses:', error);
+    throw error;
   }
+  
+
+}
   
 
 async function sendAssignments(){
@@ -64,4 +74,56 @@ async function sendAssignments(){
       
 
 }
+
+const importCanvasClasses = async (courseInput: Array<{
+  classCode: string;
+  color: string;
+  sectionId: string;
+  semesterId: string;
+}>) => {
+  try {
+    const response = await fetch('/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include', // Important for session cookies
+      body: JSON.stringify({
+        query: `
+          mutation ImportCanvasClasses($input: importCanvasClassesInput!) {
+            importCanvasClasses(input: $input) {
+              id
+              classCode
+              color
+              sections {
+                id
+                section
+                dayOfWeek
+                startTime
+                endTime
+                professor
+              }
+            }
+          }
+        `,
+        variables: {
+          input: {
+            courseInput
+          }
+        }
+      })
+    });
+
+    const data = await response.json();
+    
+    if (data.errors) {
+      throw new Error(data.errors[0].message);
+    }
+
+    return data.data.importCanvasClasses;
+  } catch (error) {
+    console.error('Error importing canvas classes:', error);
+    throw error;
+  }
+};
 
